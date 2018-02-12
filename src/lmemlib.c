840f4f54ac3f6a1bdd9dce979736315a12d08ae5
Renato Maia <maia@tecgraf.puc-rio.br>
2018-02-12 15:48:31 -0200

    Make some functions copied from Lua source as internal, not exposed in the API.

--- src/lmemlib.c
+++ src/lmemlib.c
@@ -161,44 +161,6 @@ LUAMEMLIB_API size_t luamem_checklenarg (lua_State *L, int idx)
 * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-
-/* macro to 'unsign' a character */
-#define uchar(c)	((unsigned char)(c))
-
-
-/* translate a relative string position: negative means back from end */
-LUAMEMLIB_API lua_Integer luamem_posrelat (lua_Integer pos, size_t len) {
-	if (pos >= 0) return pos;
-	else if (0u - (size_t)pos > len) return 0;
-	else return (lua_Integer)len + pos + 1;
-}
-
-LUAMEMLIB_API int luamem_str2byte (lua_State *L, const char *s, size_t l) {
-	lua_Integer posi = luamem_posrelat(luaL_optinteger(L, 2, 1), l);
-	lua_Integer pose = luamem_posrelat(luaL_optinteger(L, 3, posi), l);
-	int n, i;
-	if (posi < 1) posi = 1;
-	if (pose > (lua_Integer)l) pose = l;
-	if (posi > pose) return 0;  /* empty interval; return no values */
-	n = (int)(pose - posi + 1);
-	if (posi + n <= pose)  /* arithmetic overflow? */
-		return luaL_error(L, "string slice too long");
-	luaL_checkstack(L, n, "string slice too long");
-	for (i=0; i<n; i++)
-		lua_pushinteger(L, uchar(s[posi+i-1]));
-	return n;
-}
-
-LUAMEMLIB_API void luamem_code2char (lua_State *L, int idx, char *p, int n) {
-	int i;
-	for (i=0; i<n; ++i, ++idx) {
-		lua_Integer c = luaL_checkinteger(L, idx);
-		luaL_argcheck(L, uchar(c) == c, idx, "value out of range");
-		p[i] = uchar(c);
-	}
-}
-
-
 /*
 ** {======================================================
 ** Generic Buffer manipulation
